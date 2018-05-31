d116a9656cd53d42265a3b1e13d96f24910d5eb9
Lishat <30794115+Lishat@users.noreply.github.com>
2018-05-31 23:54:54 +0530

    Map with Form

--- app/src/main/java/com/example/subhadeep/mapintent/ScrollingActivity.java
+++ app/src/main/java/com/example/subhadeep/mapintent/ScrollingActivity.java
@@ -1,11 +1,17 @@
 package com.example.subhadeep.mapintent;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +25,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 
 public class ScrollingActivity extends AppCompatActivity implements OnMapReadyCallback{
     GoogleMap myGoogleMap;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+    private Location myLastKnownLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +36,6 @@ public class ScrollingActivity extends AppCompatActivity implements OnMapReadyCa
             setContentView(R.layout.activity_scrolling);
             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
             setSupportActionBar(toolbar);
-
             FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -65,6 +73,33 @@ public class ScrollingActivity extends AppCompatActivity implements OnMapReadyCa
         return super.onOptionsItemSelected(item);
     }
 
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
     public void initialiseMap()
     {
         MapFragment myFragment = (MapFragment)(getFragmentManager().findFragmentById(R.id.mapFragment));
@@ -94,5 +129,24 @@ public class ScrollingActivity extends AppCompatActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap)
     {
         myGoogleMap = googleMap;
+        updateLocationUI();
+    }
+    private void updateLocationUI() {
+        if (myGoogleMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                myGoogleMap.setMyLocationEnabled(true);
+                myGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                myGoogleMap.setMyLocationEnabled(false);
+                myGoogleMap.getUiSettings().setMyLocationButtonEnabled(false);
+                myLastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 }
